538e9817effcc2aa25a2004e1bd7bc07f93790f1
Ashranka <andresyovanne@gmail.com>
2021-08-16 21:53:22 -0400

    css incorporado

--- personas/webapp/views.py
+++ personas/webapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from students.models import studentsperformance
 
-
-# Create your views here.
 def home(request):
+
     return render(request, 'bienvenido.html')
+
+def nosotros(request):
+
+    return render(request, 'nosotros.html')
